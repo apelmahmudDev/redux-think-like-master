c8e5e5bac9e46b4657ed0eccbeba5a9fa5e67632
Apel Mahmud <apelmahmud.dev@gmail.com>
2023-02-24 19:10:12 +0600

    [feat] Counter increment & decrement with redux

--- script.js
+++ script.js
@@ -2,12 +2,44 @@ const incrementEl = document.getElementById('increment');
 const decrementEl = document.getElementById('decrement');
 const counterEl = document.getElementById('counter');
 
-let count = 0;
+// initial state
+const initialState = {
+	value: 0,
+};
+
+// reducer function
+const counterReducer = (state = initialState, action) => {
+	if (action.type === 'counter/increment') {
+		return {
+			...state,
+			value: state.value + 1,
+		};
+	}
+	if (action.type === 'counter/decrement') {
+		return {
+			...state,
+			value: state.value - 1,
+		};
+	}
+	return state;
+};
+
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+	const state = store.getState();
+	counterEl.innerText = state.value.toString();
+};
+
+// Update the UI with the initial data
+render();
+// And subscribe to redraw whenever the data changes in the future
+store.subscribe(render);
+
 incrementEl.addEventListener('click', () => {
-	count++;
-	counterEl.innerText = count;
+	store.dispatch({ type: 'counter/increment' });
 });
+
 decrementEl.addEventListener('click', () => {
-	count--;
-	counterEl.innerText = count;
+	store.dispatch({ type: 'counter/decrement' });
 });
